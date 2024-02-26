2d7e26743ca86dc262c6833311236e031044470f
JOHN BRANDY SERNAQUE CULQUE <john.bsculque@senacsp.edu.br>
2024-02-26 20:41:30 -0300

    Criação da model do produto

--- src/main/java/com/bits/bits/model/ProductModel.java
+++ src/main/java/com/bits/bits/model/ProductModel.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 
-public class ProducModel {
+public class ProductModel {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
